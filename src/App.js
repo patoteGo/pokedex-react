488d0e15a264e106cad9f6b8ba4dafdc2f5afb30
patodekaz <pato@dekaz.cl>
2020-04-25 22:38:00 -0400

    pokedex con estilos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import Pokecard from './components/Pokecard'
+import Pokedex from './components/Pokedex'
+
 
 function App() {
   return (
     <div className="App">
-        <Pokecard
-          id={4}
-          name="Charmainder"
-          type="fire"
-          exp={62}
-        />
+        <Pokedex/>
     </div>
   );
 }
