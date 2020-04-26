5da94bc0bb0cbe4bd265efbc99c5dd599b75bc32
patodekaz <pato@dekaz.cl>
2020-04-26 00:26:49 -0400

    pokedex con pokegame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Pokedex from './components/Pokedex'
+import Pokegame from './components/Pokegame'
 
 
 function App() {
   return (
     <div className="App">
-        <Pokedex/>
+        <Pokegame/>
     </div>
   );
 }
